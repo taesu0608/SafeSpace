9cc38ea31c0524bb464d610911c7ad52166e2585
taesu0608 <taesu0608@gmail.com>
2024-05-07 16:40:23 +0900

    feat: 1. maria db 연결 2. entity,controller,dto,repository,TestService 기능 구현

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,10 +1,10 @@
 package com.example.demo;
-
+/*
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/*
+
 @Controller
     public class MainController{
         @GetMapping("/sbb")
