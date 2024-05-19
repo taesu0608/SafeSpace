c8fd9d32dba74cd0274421f9b7ed8103e2558537
taesu0608 <taesu0608@gmail.com>
2024-05-19 19:10:55 +0900

    feat: response 모델 설계

--- src/main/java/com/example/demo/model/response/ListResult/ListResult.java
+++ src/main/java/com/example/demo/model/response/ListResult/ListResult.java
@@ -8,4 +8,8 @@ import java.util.List;
 @Getter
 @Setter
 public class ListResult<T> extends CommonResult{
+    // List 형태로 여러 데이터를 받음
+    // CommonResult를 통해 API 요청결과도 같이 출력
+
+    private List<T> list;
 }
