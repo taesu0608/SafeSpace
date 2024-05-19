6a0c83d538bf052d96f7e76910eb2b41158d1928
taesu0608 <taesu0608@gmail.com>
2024-05-19 18:38:29 +0900

    feat: API 모델링을 위한 의존성 추가

---
+++ src/main/java/com/example/demo/model/response/ListResult/ListResult.java
@@ -0,0 +1,11 @@
+package com.example.demo.model.response.ListResult;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class ListResult<T> extends CommonResult{
+}
