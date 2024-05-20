bbaa056524d55982a18ddcb0d0d9d838c4970bce
taesu0608 <taesu0608@gmail.com>
2024-05-20 15:06:48 +0900

    fix: login-Security 무시

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//로그인 (security 배제)
 @RestController
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class DemoApplication {
 
 	public static void main(String[] args) {
