c47c9bce14d811a30ff79ad2099db766c4d84b36
soyoon19 <96063941+soyoon19@users.noreply.github.com>
2024-05-27 11:08:38 +0900

    fix: SubscribeInformationController 추가 구현
    구독 정보 조회 및 구독 정보 추가하는 부분 구현

--- src/main/java/com/example/demo/controller/SubscribeInformationController.java
+++ src/main/java/com/example/demo/controller/SubscribeInformationController.java
@@ -1,8 +1,14 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.SubscribeInformationDto;
+import com.example.demo.entity.Product;
+import com.example.demo.entity.Provider;
 import com.example.demo.entity.SubscribeInformation;
+import com.example.demo.entity.WorkplaceInformation;
+import com.example.demo.service.ProductService;
+import com.example.demo.service.ProviderService;
 import com.example.demo.service.SubscribeInformationService;
+import com.example.demo.service.WorkplaceInformationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,24 +17,37 @@ import java.util.List;
 
 
 @RestController //JSON 형태로 반환하는 컨트롤러
-    @RequestMapping("/subscribe_info")
+@RequestMapping("/subscribe_info")
 @RequiredArgsConstructor
 public class SubscribeInformationController {
     private final SubscribeInformationService subscribeInformationService;
-    //private final ProductService productService;
-    //private final ProviderService providerService;
+    private final ProductService productService;
+    private final ProviderService providerService;
+    private final WorkplaceInformationService workplaceInformationService;
 
-    //구독 상품 추가
-    //TODO ProductService, ProviderService 가 만들어진 후 작성
+    //모든 구독 정보 조회
+    @GetMapping("/getAll/")
+    public List<SubscribeInformation> getAllSubscribeInformation() {
+        return subscribeInformationService.getAllSubscribeInformation();
+    }
+
+    //구독 정보 추가
     @PostMapping("/add/")
     public Boolean addSubscribeInformation(@RequestParam("uid") String uid,
                                            @RequestParam("productId") String productId) {
-//        subscribeInformationService.saveSubscribeInformation(
-//                SubscribeInformation.toEntity(
-//                        subscribeInformation,
-//
-//                )
-//        );
+        Product product = productService.getProductById(productId);
+        Provider provider = product.getProvider();
+        WorkplaceInformation workplaceInformation = workplaceInformationService.getWorkplaceInformation(uid);
+        SubscribeInformation subscribeInformation = new SubscribeInformation(uid, null, null, null,
+                null, null, workplaceInformation, product, provider);
+
+        subscribeInformationService.saveSubscribeInformation(
+                SubscribeInformation.toEntity(
+                        new SubscribeInformationDto().toDTO(subscribeInformation),
+                        provider,
+                        product
+                )
+        );
 
         return true;
     }
